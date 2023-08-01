950d9953b71e9b5fcc974235d085fa057084af92
ben <Ben@lednets.com>
2023-08-01 21:05:38 +0800

    Initial commit

---
+++ aidllearn/src/main/aidl/com/sujue/aidllearn/IMyAidlInterface.aidl
@@ -0,0 +1,10 @@
+// IMyAidlInterface.aidl
+package com.sujue.aidllearn;
+
+// Declare any non-default types here with import statements
+
+interface IMyAidlInterface {
+    void doSomething();
+    int calculateSum(int a, int b);
+    String concatenateStrings(String str1, String str2);
+}
---
+++ aidltest/src/main/java/com/sujue/aidltest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sujue.aidltest
+
+import android.content.ComponentName
+import android.content.ServiceConnection
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// 在另一个应用程序中的代码
+import com.sujue.aidltest.IMyAidlInterface;
+class MainActivity : AppCompatActivity() {
+
+    
+}
